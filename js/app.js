86b14819c10f53e99eb9e97c48bf64fee0edf28a
davidor23 <david.o.reilly@outlook.com>
2020-07-08 15:02:49 +0100

    Changes made to styling, still needs more work to fix :tada:

--- js/app.js
+++ js/app.js
@@ -8,9 +8,24 @@ $(document).ready(() => {
 
 function getMovies(searchText) {
     console.log(searchText);
-    axios.get('http://www.omdbapi.com?s=+searchText')
+    axios.get('http://www.omdbapi.com?s='+ searchText+'&apikey=69dd3150')
         .then((response) => {
-            console.log(response)
+            console.log(response);
+            let movies = response.data.Search;
+            let output = '';
+            $.each(movies, (index, movie) => {
+                output += `
+                <div class=["col-md-3">
+                <div class="well text-center">
+                <img src ="${movie.Poster}" alt ="movie poster">
+                <h5>${movie.Title}</h5>
+                <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                </div>
+               </div>
+                `;
+
+            });
+            $('#movies').html(output);
         })
     .catch((err) => {
         console.log(err);
